28a28b4b0bc79da51ffde0ec335a65a81dc9631b
JesusLarez <jesuslarez4@gmail.com>
2021-01-29 12:14:38 +0000

    Test is_1990_equals_to_MCMXC added

--- src/test/java/Test/RomanNumerals_.java
+++ src/test/java/Test/RomanNumerals_.java
@@ -1,6 +1,7 @@
 package Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import model.RomanNumerals;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,36 +12,48 @@ public class RomanNumerals_ {
     private String roman2;
     private int number2;
     private String roman3;
+    private int number3;
     private RomanNumerals romanNumerals;
+
     @Before
     public void setUp() {
         romanNumerals = new RomanNumerals();
         roman1 = "X";
         number1 = 10;
         roman2 = "XX";
-        number2= 20;
+        number2 = 20;
         roman3 = "MDCLXVI";
+        number3 = 1990;
     }
 
     @Test
     public void is_X_equals_to_ten() {
         assertThat(romanNumerals.toArabic(roman1)).isEqualTo(10);
     }
+
     @Test
     public void is_ten_equals_to_X() {
         assertThat(romanNumerals.toRoman(number1)).isEqualTo("X");
     }
+
     @Test
     public void is_XX_equals_to_twenty() {
         assertThat(romanNumerals.toArabic(roman2)).isEqualTo(20);
     }
+
     @Test
     public void is_twenty_equals_to_XX() {
         assertThat(romanNumerals.toRoman(number2)).isEqualTo("XX");
     }
+
     @Test
     public void is_MDCLXVI_equals_to_1666() {
         assertThat(romanNumerals.toArabic(roman3)).isEqualTo(1666);
+    }
+
+    @Test
+    public void is_1990_equals_to_MCMXC() {
+        assertThat(romanNumerals.toRoman(number3)).isEqualTo("MCMXC");
     }
 
 }
